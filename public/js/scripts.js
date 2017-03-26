b0eb470679044e4c7c02619dab1b1bc05a64f9b7
triyanarief <triyanarief@icloud.com>
2017-03-26 23:11:34 +0700

    update dan delete

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,5 @@
+Backbone.Model.prototype.idAttribute = '_id';
+
 // Backbone Model
 
 var Blog = Backbone.Model.extend({
@@ -64,12 +66,28 @@ var BlogView = Backbone.View.extend({
 		this.model.set('author', $('.author-update').val());
 		this.model.set('title', $('.title-update').val());
 		this.model.set('url', $('.url-update').val());
+
+		this.model.save(null, {
+			success: function(response) {
+				console.log('Successfully UPDATED blog with _id: ' + response.toJSON()._id);
+			},
+			error: function(err) {
+				console.log('Failed to update blog!');
+			}
+		});
 	},
 	cancel: function() {
 		blogsView.render();
 	},
 	delete: function() {
-		this.model.destroy();
+		this.model.destroy({
+			success: function(response) {
+				console.log('Successfully DELETED blog with _id: ' + response.toJSON()._id);
+			},
+			error: function(err) {
+				console.log('Failed to delete blog!');
+			}
+		});
 	},
 	render: function() {
 		this.$el.html(this.template(this.model.toJSON()));
